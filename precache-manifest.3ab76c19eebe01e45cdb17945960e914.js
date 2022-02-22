16b6fa116877cbdcb7638c0766a5ff9b3a247ea4
shiref211 <shirefhamam@gmail.com>
2022-02-22 14:32:33 +0200

    Updates

---
+++ precache-manifest.3ab76c19eebe01e45cdb17945960e914.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "96e83094d91540ef9e54",
+    "url": "/react-books-application/static/css/main.1763232f.chunk.css"
+  },
+  {
+    "revision": "96e83094d91540ef9e54",
+    "url": "/react-books-application/static/js/main.96e83094.chunk.js"
+  },
+  {
+    "revision": "0033e06dce4d49cee8e1",
+    "url": "/react-books-application/static/js/1.0033e06d.chunk.js"
+  },
+  {
+    "revision": "2b8900036599346b62fa",
+    "url": "/react-books-application/static/js/runtime~main.2b890003.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/react-books-application/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/react-books-application/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/react-books-application/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "e46be6c8f39542e0d2cf3cb45fc83cee",
+    "url": "/react-books-application/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shiref209/react-books-application.git/precache-manifest.9c3bb980c68c1936bb03fb8275385ecd.js"
+  "/react-books-application/precache-manifest.3ab76c19eebe01e45cdb17945960e914.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/shiref209/react-books-application.git/index.html", {
+workbox.routing.registerNavigationRoute("/react-books-application/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
